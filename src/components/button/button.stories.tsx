a682b46d5bb10d6448d53a42b881598627aea386
basurco eduardo <angelsoulsword@gmail.com>
2023-09-02 10:46:49 -0500

    feat: add storybook stories

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -13,9 +13,80 @@ type Story = StoryObj<typeof Button>;
 export const Default: Story = {
   args: {
     children: "Default",
+  },
+};
+export const Outline: Story = {
+  args: {
+    children: "Outline",
+    variant: "outline",
+  },
+};
+export const Text: Story = {
+  args: {
+    children: "Text",
+    variant: "text",
+  },
+};
+export const Primary: Story = {
+  args: {
+    children: "Primary",
+    color: "primary",
+  },
+};
+export const Secondary: Story = {
+  args: {
+    children: "Secondary",
+    color: "secondary",
+  },
+};
+export const Danger: Story = {
+  args: {
+    children: "Danger",
     color: "danger",
-    size: "sm",
+  },
+};
+export const NoBoxShadow: Story = {
+  args: {
+    children: "No Shadow",
+    size: "lg",
+    disableShadow: true,
+  },
+};
+export const Disabled: Story = {
+  args: {
+    children: "Disabled",
     disabled: true,
+  },
+};
+export const Small: Story = {
+  args: {
+    children: "Small",
+    size: "sm",
+  },
+};
+export const Medium: Story = {
+  args: {
+    children: "Medium",
+    size: "md",
+  },
+};
+export const Large: Story = {
+  args: {
+    children: "Large",
+    size: "lg",
+  },
+};
+export const StartIcon: Story = {
+  args: {
+    children: "Start Icon",
+    size: "lg",
     StartIcon: Logo,
+  },
+};
+export const EndIcon: Story = {
+  args: {
+    children: "End Icon",
+    size: "lg",
+    EndIcon: Logo,
   },
 };
